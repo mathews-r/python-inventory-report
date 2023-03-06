47c5205e241f2d5c7d1c4aff66609d621886a1ee
Mathews <mathewsrodrigues9@gmail.com>
2023-03-06 09:01:14 -0300

    funcao para gerar relatorio simples de csv e json ok

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,25 +1,25 @@
 from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
 import csv
 import json
-import xml.etree.ElementTree as Et
+import xml.etree.ElementTree as ET
 
 
 class Inventory:
     @classmethod
-    def import_data(cls, path, type="completo"):
+    def import_data(cls, path, type):
         report = ""
-        report2 = ""
         if "csv" in path:
             report = cls.read_csv(path)
-            report2 = SimpleReport.generate(report)
         elif "json" in path:
             report = cls.read_json(path)
-            report2 = SimpleReport.generate(report)
         elif "xml" in path:
             report = cls.read_xml(path)
-            report2 = SimpleReport.generate(report)
 
-        return report2
+        if type == "simples":
+            return SimpleReport.generate(report)
+        else:
+            return CompleteReport.generate(report)
 
     @classmethod
     def read_csv(cls, path):
@@ -28,17 +28,16 @@ class Inventory:
             data_list = []
             for data_csv in data:
                 data_list.append(data_csv)
-
         return data_list
 
     @classmethod
     def read_json(cls, path):
-        with open(path, mode="r") as file:
+        with open(path, mode="r", encoding="utf-8") as file:
             data = json.load(file)
         return data
 
     @classmethod
     def read_xml(cls, path):
         with open(path, mode="r", encoding="utf-8") as file:
-            xml = Et.parse(file)
+            xml = ET.parse(file)
         return xml
